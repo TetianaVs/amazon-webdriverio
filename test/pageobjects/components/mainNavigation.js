ead0d8af1da173f94b8a69ee91dc9cb3f8bbd849
TetianaVs <tetiana.qait@gmail.com>
2022-08-24 18:35:09 +0300

    Add main navigation selectors and functions

--- test/pageobjects/components/mainNavigation.js
+++ test/pageobjects/components/mainNavigation.js
@@ -49,16 +49,15 @@ class MainNavigation extends Page {
     return $('#nav-al-title');
   }
 
-  get createListLink() {
-    return $('#nav-al-wishlist > a:nth-child(2) > span');
+  // Select specific Your Lists action
+  selectYourListsAction(index) {
+    return $(`#nav-al-wishlist > a:nth-child(${index})> span`);
   }
 
-  get findListRegistryLink() {
-    return $('#nav-al-wishlist > a:nth-child(3) > span');
-  }
-
-  get charityListLink() {
-    return $('#nav-al-wishlist > a:nth-child(4) > span');
+  // Click on specific Your List: Create a List{2}, Find a List or Registry{3}, Amazon Smile Charity Lists{4}.
+  // @param {Number} index  - the specific index of wish lists actions.
+  async clickYourListsAction(index) {
+    await this.selectYourListsAction(index).click();
   }
 
   get yourAccountTitle() {
@@ -70,7 +69,7 @@ class MainNavigation extends Page {
   }
 
   get ordersLink() {
-    return $('#nav-al-your-account > a:nth-child(2) > span');
+    return $('#nav_prefetch_yourorders > span');
   }
 
   get navCart() {
@@ -97,10 +96,13 @@ class MainNavigation extends Page {
     return $('//*[@id="nav-xshop"]/a[4]');
   }
 
+  // Select specific xShop element
   xShopGiftCardsList(index) {
     return $(`#nav-xshop a:nth-child(${index})`);
   }
 
+  // select specific xShop element: Today's Deals{2}, Customer Service{3}, Registry{4}, Gift Cards{5}, Sell{6}
+  // @param {Number} index  - the specific index of xShop element
   async clickXshopItem(index) {
     await this.xShopGiftCardsList(index).click();
   }
